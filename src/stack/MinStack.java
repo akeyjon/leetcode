fec2b147428471c5580f4c33e082e08575715fd1
dailongwen <1181802528@qq.com>
2019-04-17 22:50:50 +0800

    栈结构题

---
+++ src/stack/MinStack.java
@@ -0,0 +1,55 @@
+//设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+//
+//
+// push(x) -- 将元素 x 推入栈中。
+// pop() -- 删除栈顶的元素。
+// top() -- 获取栈顶元素。
+// getMin() -- 检索栈中的最小元素。
+//
+//
+// 示例:
+//
+// MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+//
+//
+
+package stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MinStack {
+
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current
+        // minimum value changes after pushing the new value x
+        if(x <= min){
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value,
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
